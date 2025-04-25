5ea6a9c3c0b6b3b4e64ac9ea3d4bc3c64087b3f9
Tzahi Argaman <tzahi.argaman@cyberark.com>
2025-04-25 15:06:46 +0300

    cdk

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,73 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as logs from 'aws-cdk-lib/aws-logs';
+import * as path from 'path';
 
-export class CdkStack extends cdk.Stack {
+export class Wiki7CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC with isolated networking and NAT for private resources
+    const vpc = new ec2.Vpc(this, 'Wiki7Vpc', {
+      maxAzs: 2,
+      natGateways: 1,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'public',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: 'private',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // ECS cluster within the VPC
+    const cluster = new ecs.Cluster(this, 'Wiki7Cluster', {
+      vpc,
+      containerInsightsV2: ecs.ContainerInsights.ENHANCED,
+    });
+
+    // IAM role used by ECS tasks (application-level permissions)
+    const taskRole = new iam.Role(this, 'Wiki7TaskRole', {
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+      description: 'IAM role for MediaWiki containers',
+    });
+
+    taskRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonECSTaskExecutionRolePolicy')
+    );
+
+    // Log group for ECS containers
+    const logGroup = new logs.LogGroup(this, 'Wiki7LogGroup', {
+      retention: logs.RetentionDays.ONE_MONTH,
+    });
+
+    // Fargate Task Definition for MediaWiki container
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'Wiki7TaskDef', {
+      cpu: 512,
+      memoryLimitMiB: 1024,
+      taskRole,
+    });
+    
+    taskDefinition.addContainer('MediaWikiContainer', {
+      image: ecs.ContainerImage.fromAsset(path.join(__dirname, '../../docker')),
+      logging: ecs.LogDriver.awsLogs({
+        logGroup,
+        streamPrefix: 'mediawiki',
+      }),
+      environment: {
+        MEDIAWIKI_DB_HOST: 'db-host-placeholder',
+        MEDIAWIKI_DB_NAME: 'wikidb',
+        MEDIAWIKI_DB_USER: 'wikiuser',
+        MEDIAWIKI_DB_PASSWORD: 'secret',
+      },
+    });
   }
 }
